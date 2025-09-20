5593704c4dcee6d3bcb3bb3794770375d8772b1c
codingchamp27 <harshk4ever@gmail.com>
2025-09-20 23:22:33 +0530

    Unstop Link

--- Frontend/src/app/_components/AboutSection.tsx
+++ Frontend/src/app/_components/AboutSection.tsx
@@ -7,7 +7,7 @@ export default function AboutSection(){
                 <h1 className="text-[36px] leading-[45px] my-2 font-normal font-dragon text-2xl bg-gradient-to-b from-[#5A3E17] via-[#FFF5B6] to-[#D4AF37] bg-clip-text text-transparent">ABOUT GAMBITOR</h1>
                 <p className="text-[20px] font-overpass leading-[25px]  font-medium text-justify mb-8 text-[#FAF4BE]">
                 GambitoR is a prestigious national-level exam tailored specifically for students from classes 9th to 12th who strive for excellence and have a passion for tackling and conquering challenges. This unique competition evaluates students' logical and analytical skills through questions designed by our student community, all within the exciting and adventurous theme of Harry Potter. Organized by the Outreach Cell of IIT Roorkee, this year marks the fourth edition of GambitoR, promising to bring out the best in each participant. Get ready to sail on this adventure and discover what you’re truly capable of!                </p>
-                <a href="/signup">
+                <a href="https://unstop.com/competitions/gambitor-40-national-school-championship-iit-roorkee-1560754?lb=xEISBOSc">
                 <Animations>
                     <button className="mt-4 transition ease-out duration-300 bg-[#C9A454] border-4 border-[#C37227] rounded-full xl:w-[261px] w-[20vw] text-[20px] leading-[25px] text-[#A50034] font-bold h-[60px] cursor-pointer hover:bg-[#C49B44] hover:border-[#BC6E26] hover:shadow-[-1px_1px_4px_5px_rgba(188,110,38,0.30)] active:bg-[#B28B38] active:shadow-[-1px_1px_4px_5px_rgba(188,110,38,0.30)] active:border-[#AA6322] font-dragon tracking-wider	">REGISTER NOW !</button>
 
